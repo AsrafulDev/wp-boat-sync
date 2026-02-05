47c30a29ac2afe1431e9c39f46075007f372337c
MD ASRAFUL ISLAM SHAKIL <dpadpro24@gmail.com>
2026-02-06 02:02:30 +0600

    * Version: 1.0.1

--- wp-boat-sync.php
+++ wp-boat-sync.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Boat Sync
  * Description: Sync Boats.com inventory into WordPress (CPT + meta + custom tables) with scheduled async sync and manual tools.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Plugin URI: https://github.com/AsrafulDev/wp-boat-sync
@@ -21,7 +21,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('WPBS_VERSION', '1.0.0');
+define('WPBS_VERSION', '1.0.1');
 define('WPBS_PLUGIN_FILE', __FILE__);
 define('WPBS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('WPBS_PLUGIN_URL', plugin_dir_url(__FILE__));
